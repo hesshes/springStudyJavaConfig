62fc563cc041b5dd4b4907470384fa6c0c0ea168
hessh <hessh@taeu-dev-01>
2023-01-27 12:30:27 +0900

    코배스 자바 설정 // ~ 88 page

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,12 +1,44 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariJNDIFactory;
+
+import lombok.extern.log4j.Log4j;
 
 //applicationContext or bean factory 가 사용할 설정 정보라는 의미
-//
-@Configuration
-@ComponentScan(basePackages = {"org.zerock.sample"})
+@Configuration // <beans> </beans>
+@PropertySource("classpath:/config/globals.properties")
+@ComponentScan(basePackages = { "org.zerock.sample" })
 public class RootConfig {
+
+	@Autowired
+	Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		hikariConfig.setJdbcUrl(env.getProperty("jdbc.url"));
+		hikariConfig.setUsername(env.getProperty("jdbc.username"));
+		hikariConfig.setPassword(env.getProperty("jdbc.password"));
+
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+		System.out.println(env.toString());
+		return dataSource;
+
+	}
 
 }
